a9934e65929ed180b59e26b3d11e35733a154d9c
Kevin Chard <kevin.chard@tapjoy.com>
2017-09-13 11:41:05 -0700

    add bin template script

---
+++ bin/template.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+# set default for flags
+someflag=false
+
+# parse flags
+while getopts ":f" opt; do
+    case ${opt} in
+        f ) someflag=true
+            ;;
+        \? ) echo "Usage: command [-c] args"
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+# set remaining args
+bar=$1
+
+# define functions
+function foo {
+    echo "FOO"
+}
+
+# execute flags
+if [ "$someflag" = true ] ; then
+    foo
+fi
+
+# main logic
+echo $bar
